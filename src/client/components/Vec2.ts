07da456ef5fdab7628f126875188d78797514da4
Adil <adilserkerov@gmail.com>
2019-08-12 18:45:47 +0300

    fix linter

--- src/client/components/Vec2.ts
+++ src/client/components/Vec2.ts
@@ -4,12 +4,12 @@ export interface IVec2 {
 }
 
 export class Vec2 implements IVec2 {
-    public x: number;
-    public y: number;
+	public x: number;
+	public y: number;
 
-    public constructor(x: number = 0, y: number = 0) {
-        this.x = x;
-	    this.y = y;
+	public constructor(x: number = 0, y: number = 0) {
+		this.x = x;
+		this.y = y;
 	}
 
 	public set(v: Vec2) {
@@ -19,19 +19,19 @@ export class Vec2 implements IVec2 {
 		return this;
 	}
 
-    public setX(x: number) {
+	public setX(x: number) {
 		this.x = x;
 
 		return this;
-    }
-    
-    public setY(y: number) {
+	}
+
+	public setY(y: number) {
 		this.y = y;
 
 		return this;
-    }
-    
-    public copy(v: Vec2) {
+	}
+
+	public copy(v: Vec2) {
 		this.x = v.x;
 		this.y = v.y;
 
@@ -63,16 +63,16 @@ export class Vec2 implements IVec2 {
 		this.y += s;
 
 		return this;
-    }
-    
-    public addScaledVector(v: Vec2, s: number) {
+	}
+
+	public addScaledVector(v: Vec2, s: number) {
 		this.x += v.x * s;
 		this.y += v.y * s;
 
 		return this;
-    }
-    
-    public multiply(v: Vec2) {
+	}
+
+	public multiply(v: Vec2) {
 		this.x *= v.x;
 		this.y *= v.y;
 
@@ -84,8 +84,8 @@ export class Vec2 implements IVec2 {
 		this.y *= scalar;
 
 		return this;
-    }
-    
+	}
+
 
 	public negate() {
 		this.x = - this.x;
@@ -96,20 +96,20 @@ export class Vec2 implements IVec2 {
 
 	public dot(v: Vec2) {
 		return this.x * v.x + this.y * v.y;
-    }
-    
-    public length() {
-		return Math.sqrt( this.x * this.x + this.y * this.y );
-    }
+	}
 
-    public divideScalar(scalar: number) {
-		return this.multiplyScalar( 1 / scalar );
+	public length() {
+		return Math.sqrt(this.x * this.x + this.y * this.y);
 	}
-    
-    public normalize() {
-		return this.divideScalar( this.length() || 1 );
+
+	public divideScalar(scalar: number) {
+		return this.multiplyScalar(1 / scalar);
 	}
-	
+
+	public normalize() {
+		return this.divideScalar(this.length() || 1);
+	}
+
 	public equal(v: Vec2): boolean {
 		return this.x === v.x && this.y === v.y;
 	}
