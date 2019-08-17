fecaf85cf883ade0c6e1a29e7a328e8c48b46dda
Adil <adilserkerov@gmail.com>
2019-08-17 19:05:10 +0300

    fix by heroku

--- src/client/api/PersonApi.ts
+++ src/client/api/PersonApi.ts
@@ -4,7 +4,7 @@ import { PersonForm, PersonData } from "../types/TreeTypes";
 export class PersonApi {
 
     static async createPerson(data: PersonForm): Promise<PersonData> {
-        const response = await fetch(`http://${window.location.hostname}:8080/api/person/`, {
+        const response = await fetch(`https://${window.location.hostname}/api/person/`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(data)
@@ -14,7 +14,7 @@ export class PersonApi {
     }
 
     static async addChild(data: PersonForm): Promise<PersonData> {
-        const URL = `http://${window.location.hostname}:8080/api/person/${data.parentId}/child`;
+        const URL = `https://${window.location.hostname}/api/person/${data.parentId}/child`;
         const response = await fetch(URL, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -25,7 +25,7 @@ export class PersonApi {
     }
 
     static async addParent(data: PersonForm): Promise<PersonData> {
-        const URL = `http://${window.location.hostname}:8080/api/person/parent`;
+        const URL = `https://${window.location.hostname}/api/person/parent`;
         const response = await fetch(URL, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -36,7 +36,7 @@ export class PersonApi {
     }
 
     static async addRelationship(data: PersonRelationshipForm): Promise<PersonData> {
-        const URL = `http://${window.location.hostname}:8080/api/person/${data.relationship}/relationship`;
+        const URL = `https://${window.location.hostname}/api/person/${data.relationship}/relationship`;
         const response = await fetch(URL, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -47,7 +47,7 @@ export class PersonApi {
     }
 
     static async getPersonList(params: { ids: string[] }): Promise<PersonData[]> {
-        const url = `http://${window.location.hostname}:8080/api/person?ids=${
+        const url = `https://${window.location.hostname}/api/person?ids=${
             params.ids.reduce((a, i) => { return a + i + ',' }, '').slice(0, -1)
         }`;
 
